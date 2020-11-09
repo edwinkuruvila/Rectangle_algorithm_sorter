98895108d34e6fdff2297587d7559f5bf3644deb
Edwin Kuruvila <edwinkuruvila10@gmail.com>
2020-11-08 22:24:20 -0600

    Update rectsorter.py

--- rectsorter.py
+++ rectsorter.py
@@ -9,23 +9,23 @@ root = Tk()
 W, H = 500, 600
 rect_position = {}
 SortType = StringVar()
-SorterOPTS = ['Quick Sort', 'Selection Sort']
+SorterOPTS = ['Quick Sort', 'Selection Sort', 'Bubble Sort']
 root.title('Rectangle Organizer')
-root.geometry(("{}x300").format(W))
+root.geometry(("{}x{}").format(W, H//2))
 root.resizable(0, 0)
-SortType.set(SorterOPTS[1])
-global runAmount
+SortType.set(SorterOPTS[2])
 runAmount = 0
+runCycle_EndIND = 0
+
 
 # ----- Inputs # of rectangles from user -----#
 Label(root, text="Number of Rectangles: ").grid(
     row=0, column=0, sticky='W')
-Num_rect = Entry(root, width=5)
+Num_rect = Entry(root, width=10)
 Num_rect.grid(row=0, column=1, sticky='W')
 
+
 # ----- Creating rectangles -----#
-
-
 def draw_rect(Tsorter, WHICHrect=-1):
     canvas.delete("MyR")
     NR = int(Num_rect.get())
@@ -37,7 +37,6 @@ def draw_rect(Tsorter, WHICHrect=-1):
                 rect_startPos, rect_position[RHassign], rect_startPos+width_rectangles, canvas_height, tags='MyR')
             rect_startPos += width_rectangles
     if Tsorter and WHICHrect != -100:
-        WHICHrect = WHICHrect-1
         for RHassign in range(NR):
             if RHassign == WHICHrect:
                 canvas.create_rectangle(
@@ -59,9 +58,8 @@ def create_rectsN():
         rect_position[RHassign] = random.randint(11, canvas_height-10)
     draw_rect(False)
 
+
 # ----- Sorting rectangles -----#
-
-
 def sortingList():
     global SSsorting
     SSsorting = []
@@ -72,7 +70,6 @@ def sortingList():
 def selectionSort():
     global runAmount
     num = runAmount
-    visual = num
     for i in range(NR):
         rect_position[i] = SSsorting[i]
     root.after(20, draw_rect, True, num)
@@ -80,22 +77,47 @@ def selectionSort():
         if SSsorting[numchecker] > SSsorting[num]:
             num = numchecker
     root.after(10, draw_rect, True, num)
-    for i in range(NR):
-        rect_position[i] = SSsorting[i]
-    root.after(0, draw_rect, True, visual)
     SSsorting[runAmount], SSsorting[num] = SSsorting[num], SSsorting[runAmount]
     runAmount += 1
     if runAmount == NR:
         runAmount = 0
-        root.after(100, draw_rect, False, -100)
+        root.after(20, draw_rect, False, -100)
         return
     root.after(25, selectionSort)
+
+
+def bubbleSort():
+    global runAmount
+    global runCycle_EndIND
+    for i in range(NR):
+        rect_position[i] = SSsorting[i]
+    root.after(5, draw_rect, True, runAmount+1)
+    if SSsorting[runAmount] < SSsorting[runAmount+1]:
+        SSsorting[runAmount], SSsorting[runAmount +
+                                        1] = SSsorting[runAmount+1], SSsorting[runAmount]
+        runCycle_EndIND += 1
+    for i in range(NR):
+        rect_position[i] = SSsorting[i]
+    root.after(0, draw_rect, True, runAmount+1)
+    if runCycle_EndIND == 0 and runAmount == NR-2:
+        for i in range(NR):
+            rect_position[i] = SSsorting[i]
+        runAmount = 0
+        runCycle_EndIND = 0
+        root.after(5, draw_rect, False, -100)
+        return
+    runAmount += 1
+    if runAmount == NR-1:
+        runAmount = 0
+        runCycle_EndIND = 0
+    root.after(5, bubbleSort)
 
 
 def sort():
     global NR
     NR = int(Num_rect.get())
     sortingList()
+    errorLabel.configure(text=' ')
     if SortType.get() == 'Quick Sort':
         SSsorting.sort(reverse=True)
         for i in range(NR):
@@ -103,6 +125,13 @@ def sort():
         draw_rect(False, -100)
     elif SortType.get() == 'Selection Sort':
         selectionSort()
+    elif SortType.get() == 'Bubble Sort':
+        if int(Num_rect.get()) > 30:
+            errorLabel.configure(text='Error:TooMany!')
+        elif int(Num_rect.get()) == 1:
+            errorLabel.configure(text='Error:NotEnough!')
+        else:
+            bubbleSort()
 
 
 # ----- Menu Items -----#
@@ -110,6 +139,9 @@ Button(root, text='Draw New Rectangles', command=create_rectsN).grid(
     row=1, column=0, sticky='W')
 OptionMenu(root, SortType, *SorterOPTS).grid(row=0, column=2)
 Button(root, text="Sort", command=sort).grid(row=1, column=2)
+errorLabel = Label(root)
+errorLabel.grid(row=1, column=1)
+
 
 # ----- Canvas for creating rectangles -----#
 canvas_width = W
